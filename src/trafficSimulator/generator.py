494a081a05887561a4d181adc0b1e3ea5eecaf8b
MichalZelasko <zelaskomj@gmail.com>
2023-03-04 22:25:56 +0100

    Constant car generator
    Now cars are generated during the whole simulation. Intensity of generating cars is determined by intensity_function (which is given to simulation as a argument).

--- src/trafficSimulator/generator.py
+++ src/trafficSimulator/generator.py
@@ -2,35 +2,64 @@ import numpy as np
 
 class Generator :
 
-    def __init__(self, carTypes, simulation) :
+    def __init__(self, carTypes, paths, simulation) :
         self.sum = 0
+        self.pathSum = 0
         self.number = 0
         self.typesWithLikelihood = []
+        self.pathsWithLikelihood = []
+        self.summary = {}
+        self.pathSummary= {}
         self.simulation = simulation
         for type in carTypes :
             self.typesWithLikelihood.append((self.sum, type[1]))
             self.sum += type[0]
+        for path in paths :
+            self.pathsWithLikelihood.append((self.pathSum, path[1]))
+            self.pathSum += path[0]
+
+    def chooseType(self) :
+        likelihood = np.random.uniform(0, self.sum)
+        a, b = 0, len(self.typesWithLikelihood)
+        while(a < b - 1) :
+            if(self.typesWithLikelihood[(a + b) // 2][0] < likelihood) :
+                a = (a + b) // 2
+            else :
+                b = (a + b) // 2
+        if a in self.summary.keys() :
+            self.summary[a] += 1
+        else :
+            self.summary[a] = 1
+        return a
+    
+    def choosePath(self) :
+        likelihood = np.random.uniform(0, self.pathSum)
+        a, b = 0, len(self.pathsWithLikelihood)
+        while(a < b - 1) :
+            if(self.pathsWithLikelihood[(a + b) // 2][0] < likelihood) :
+                a = (a + b) // 2
+            else :
+                b = (a + b) // 2
+        if a in self.pathSummary.keys() :
+            self.pathSummary[a] += 1
+        else :
+            self.pathSummary[a] = 1
+        return a
 
     def generateCars(self, n) :
         carTypes = []
-        summary = {}
         for _ in range(n) :
             self.number += 1
-            likelihood = np.random.uniform(0, self.sum)
-            a, b = 0, len(self.typesWithLikelihood)
-            while(a < b - 1) :
-                if(self.typesWithLikelihood[(a + b) // 2][0] < likelihood) :
-                    a = (a + b) // 2
-                else :
-                    b = (a + b) // 2
-            if a in summary.keys() :
-                summary[a] += 1
-            else :
-                summary[a] = 1
+            a = self.chooseType()
+            b = self.choosePath()
             carType = self.typesWithLikelihood[a][1]
             carType["id"] = self.number
+            carType["path"] = self.pathsWithLikelihood[b][1]
+            carType["position"] = np.random.uniform(0, self.simulation.roads[carType["path"][0]].length)
             carTypes.append(({}, carType))
-            print(summary)
+            
+            print(self.pathsWithLikelihood[b][1], carType["position"])
+            # print(self.summary)
         self.simulation.set_vehicles(carTypes)
 
         
--- src/trafficSimulator/simulation.py
+++ src/trafficSimulator/simulation.py
@@ -1,6 +1,7 @@
 from .road import Road
 from .car import Car
 import time
+import numpy as np
 
 
 class Simulation:
@@ -8,6 +9,8 @@ class Simulation:
         self.roads = {}
         self.cars = []
         self.speed = speed
+        self.simulation_time = 0.0
+        self.generator = None
 
     def create_car(self, conf, param) :
         self.cars.append(Car(simulation=self, conf=conf, parameters=param))
@@ -26,6 +29,10 @@ class Simulation:
             print(road)
             self.create_road(road[0], road[1], road[2])
 
+    def set_generator(self, generator, intensityFuntion) :
+        self.generator = generator
+        self.intensity_function = intensityFuntion
+
     def run(self, steps) :
         start = time.time()
         end = time.time()
@@ -34,8 +41,12 @@ class Simulation:
             end = start
             start = time.time()            
             dt = max(0.001, start - end)
+            self.simulation_time += dt
             for i in range(1000000) :
                 i += 1
             for road in self.roads.values() :
                 road.move_cars(dt = dt * self.speed)
+            if self.generator != None and np.random.uniform(0, 1.0) <= self.speed * self.intensity_function(self.simulation_time) * dt : 
+                self.generator.generateCars(1)
+            
             
